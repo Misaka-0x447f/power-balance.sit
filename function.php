e85b977f41a7fa5d45d79edc806276cd414237b8
苍崎橙子 <AozakiOrenji@outlook.com>
2017-03-23 13:46:19 +0800

    fix var issue

--- function.php
+++ function.php
@@ -1,35 +1,42 @@
 <?php
+/* class dataOpt
+ * 向预先定义的文件(eleBalance.data)写入数据或读取数据。
+ * 每行包括两个数据：记录时间和电费余额。
+ * 数据的最大行数已在$lengthLimit中定义。
+ * 接口函数列表：
+ * push -- 将输入插入到文件尾。
+ * ls -- 列出数据。
+ */
 class dataOpt{
     private $lengthLimit = 30;
     private $f;
-    public function push($time, $balance);
-    public function ls();
     function __construct(){
-        $f = fopen("eleBalance.data","r+");
-        if($f == false){
+        $this->f = fopen("eleBalance.data","r+");
+        if($this->f == false){
             exit("用户错误：failed to open file 'eleBalance.data' in mode 'r+'");
         }
     }
     function __destruct(){
-        if(fclose($f)){
+        if(fclose($this->f)){
             exit("用户错误：failed to open file eleBalance.data");
         }
     }
     public function push($time, $balance){
         //将新数据push到文件中。如果超过限制，丢弃最早的一行。
-        $content = file($f);
+        $content = file($this->f);
         array_push($content, array($time, $balance));
-        if(count($content) >= $lengthLimit){
+        if(count($content) >= $this->lengthLimit){
             $pos = 1; //指定写入开始点:要丢弃第一行吗？
         }else{
             $pos = 0;
         }
         for(;$pos<count($content);$pos++){
-            fprintf($f, "%s\t%s\n", $content[$pos][0], $content[$pos][1]);
+            fprintf($this->f, "%s\t%s\n", $content[$pos][0], $content[$pos][1]);
         }
         return true;
     }
     public function ls(){
-        $tableOfBalance = fscanf($f, "%s\t%s");
+        //incomplete
+//        $this->tableOfBalance = fscanf($this->f, "%s\t%s");
     }
 }
