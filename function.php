280971b92add3bbbe49ed10ddb9571d87482f21a
苍崎橙子 <AozakiOrenji@outlook.com>
2017-03-23 19:49:57 +0800

    complete function ls()

--- function.php
+++ function.php
@@ -19,26 +19,28 @@
  *          Description
  *              列出完整的30个数据。
  *          Return
- *              如果执行成功，返回true。
+ *              如果执行成功，返回得到的数据。
  *              如果遇到意外错误，脚本将退出。
  */
 class dataOpt{
     private $lengthLimit = 30;
-    private $f;
+    private $filePointer;
+    private $fileName = "eleBalance.data";
     function __construct(){
-        $this->f = fopen("eleBalance.data","r+");
-        if($this->f == false){
-            exit("用户错误：Failed to open file 'eleBalance.data' in mode 'r+'");
+        $this->filePointer = fopen($this->fileName,"r+");
+        if($this->filePointer == false){
+            exit("用户错误：Failed to open file '" . $this->fileName . "' in mode 'r+'");
         }
     }
     function __destruct(){
-        if(fclose($this->f)){
-            exit("用户错误：Failed to open file eleBalance.data");
+        if(fclose($this->filePointer)){
+            exit("用户错误：Failed to open file " . $this->fileName);
         }
     }
     public function push($time, $balance){
-        $content = file($this->f,FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES); //逐行读取。将跳过空行，并且不会将换行符读入。
-        rewind($this->f); //重置文件指针到文件头，准备好写入。
+        rewind($this->filePointer); //重置文件指针到文件头。
+        $content = file($this->fileName,FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES); //逐行读取。将跳过空行，并且不会将换行符读入。
+        rewind($this->filePointer); //重置文件指针到文件头，准备好写入。
         array_push($content, array($time, $balance));
         if(count($content) >= $this->lengthLimit){
             $pos = 1; //指定写入开始点:要丢弃第一行吗？
@@ -46,12 +48,18 @@ class dataOpt{
             $pos = 0;
         }
         for(;$pos<count($content);$pos++){
-            fprintf($this->f, "%s\t%s\n", $content[$pos][0], $content[$pos][1]);
+            if(fprintf($this->filePointer, "%s\t%s\n", $content[$pos][0], $content[$pos][1]) == 0){
+                echo "用户警告：Failed to write file " . $this->fileName;
+            }
         }
         return true;
     }
     public function ls(){
-        //incomplete
-//        $this->tableOfBalance = fscanf($this->f, "%s\t%s");
+        rewind($this->filePointer); //重置文件指针到文件头。
+        $tableOfContent = file($this->fileName,FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES); //逐行读取。将跳过空行，并且不会将换行符读入。
+        for($pos = 0;$pos < count($tableOfContent);$pos++){
+            $tableOfContent[$pos] = explode("\t",$tableOfContent[$pos]);
+        }
+        return $tableOfContent;
     }
 }
