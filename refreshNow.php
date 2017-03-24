e870047c24f25bc52a2fd09d0b479e5ebdce1607
苍崎橙子 <AozakiOrenji@outlook.com>
2017-03-24 16:07:15 +0800

    Initial a program which will be used to refresh balance.

---
+++ refreshNow.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Aozak
+ * Date: 2017/3/24
+ * Time: 15:28
+ *
+ * This program will refresh balance now.
+ */
+require("function.php");
+$op = new dataOp();
+//$op->push(time(),balance);
+unset($op);
