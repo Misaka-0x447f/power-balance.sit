b4286f369ebb69f10ef2a1b8c8c88896b017b3d8
苍崎橙子 <AozakiOrenji@outlook.com>
2017-03-27 19:58:53 +0800

    rm canvas.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function updateData(){
             dataType: "html",
             url: "getFeeInfo.php?noCache=" + Math.random(),
             success: function (data) {
-                sto = eval(data);
+                sto = JSON.parse(data);
             },
             timeout: 30000,
             error: function (XMLHttpRequest, textStatus, errorThrown) {
@@ -17,8 +17,30 @@ function updateData(){
         }
     )
 }
-var sto = {};
+var sto;
 $(document).ready(function(){
     updateData();
-    draw();
+    preLoadImg();
+    $("#container").imagesLoaded(function(){
+       draw();
+    });
 });
+var imgSto = {};
+function preLoadImg(){
+    imgSto[0] = new Image();
+    imgSto[1] = new Image();
+    imgSto[0].src = "res/bg.png";
+    imgSto[1].src = "res/bar.png";
+}
+function draw(){
+    var canvas = document.getElementById("canvas");
+    if(!canvas.getContext){
+        canvas.innerHTML = "警告：你的浏览器不支持canvas。";
+    }else{
+        var ctx = canvas.getContext("2d");
+        ctx.font = "48px 7barSp";
+        ctx.fillText(sto["bal"],200,50);
+        ctx.fillText(sto["est"],200,100);
+        ctx.drawImage(imgSto[0],0,0);
+    }
+}
