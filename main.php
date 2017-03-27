615aacca60cc65f7c3b384e9a3f5ca0caec9b666
苍崎橙子 <AozakiOrenji@outlook.com>
2017-03-27 14:17:39 +0800

    It's magic!

--- main.php
+++ main.php
@@ -3,9 +3,11 @@
  * This program is designed for returning post request of balance and estimated time to balance used up to webPages
  */
 
-//require everything
 require("function.php");
 
 $op = new dataOp();
 echo $op->getBalance() . "," . $op->getEstRem();
 unset($op);
+
+//How did it done?
+//It's magic!
