736e9ffb90c2d8e7bf839da30be19d188fdc8fbd
苍崎橙子 <AozakiOrenji@outlook.com>
2017-03-27 16:47:44 +0800

    reformat data to json

--- getFeeInfo.php
+++ getFeeInfo.php
@@ -6,11 +6,21 @@
 require("function.php");
 
 $op = new dataOp();
-if($op->getEstRem() == false){
-    echo $op->getBalance() . ",false";
+$data = Array(
+    "bal" => $op->getBalance(),
+    "est" => $op->getEstRem(),
+    "prg" => 0
+);
+if($data["est"] == false){
+    $data["est"] = "---";
+    $data["prg"] = $data["bal"] / 100; //满值为100度
 }else{
-    echo $op->getBalance() . "," . $op->getEstRem();
+    $data["prg"] = $data["est"] / 30; //满值为30天
 }
+if($data["prg"] > 1){
+    $data["prg"] = 1;
+}
+echo json_encode($data);
 unset($op);
 
 //How did it done?
